7098ce7ab78bf3b25ea723f5f3c7b41f19335874
Sean Soo <64569228+ssjh23@users.noreply.github.com>
2024-03-17 23:52:14 +0800

    Delete Assignment2/ensign-shopping-ws directory

--- Assignment2/ensign-shopping-ws/src/screens/LandingPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function LandingPage() {
-  return (
-    <div>
-      <h1>Ensign Shopping</h1>
-      <p>Welcome to Ensign Shopping, your one-stop shop for all your needs!</p>
-    </div>
-  )
-}
